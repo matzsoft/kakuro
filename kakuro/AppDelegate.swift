1d854eb8085799456a53d6552328a7fefeeee0f7
Mark Johnson <markj@matzsoft.com>
2018-04-04 20:20:18 -0700

    Initial Commit

---
+++ kakuro/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  kakuro
+//
+//  Created by Mark Johnson on 4/4/18.
+//  Copyright © 2018 matzsoft. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ kakuro/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  kakuro
+//
+//  Created by Mark Johnson on 4/4/18.
+//  Copyright © 2018 matzsoft. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
