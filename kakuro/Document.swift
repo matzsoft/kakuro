734479ca2063d6a4af2e225f5ac55d7c736c2a35
Mark Johnson <markj@matzsoft.com>
2019-03-06 17:56:52 -0800

    Converted to Swift 4.2.

--- kakuro/Document.swift
+++ kakuro/Document.swift
@@ -44,8 +44,8 @@ class Document: NSDocument, NSSpeechSynthesizerDelegate {
 
     override func makeWindowControllers() {
         // Returns the Storyboard that contains your Document window.
-        let storyboard = NSStoryboard(name: NSStoryboard.Name("Main"), bundle: nil)
-        let windowController = storyboard.instantiateController(withIdentifier: NSStoryboard.SceneIdentifier("Document Window Controller")) as! NSWindowController
+        let storyboard = NSStoryboard(name: "Main", bundle: nil)
+        let windowController = storyboard.instantiateController(withIdentifier: "Document Window Controller") as! NSWindowController
         self.addWindowController(windowController)
         
         let controller = windowController.contentViewController as! ViewController
